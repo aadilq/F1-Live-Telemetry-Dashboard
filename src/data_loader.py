15396fd797a275f0e34343f3fe8b07f7e617e6c1
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2026-02-23 14:13:35 -0500

    define function to return fastest lap

--- src/data_loader.py
+++ src/data_loader.py
@@ -6,8 +6,18 @@ fastf1.Cache.enable_cache('data/')
 def get_session(year, event, session_type):
     session = fastf1.get_session(year=year, gp=event, identifier=session_type)
     session.load()
+    return session
 
 
 # session = get_session(2024, 'Monza', 'Q')
 # print(session)
+
+def get_fast_lap(session, driverNumber):
+    laps = session.laps.pick_driver(driverNumber)
+    fastest_lap = laps.pick_fastest()
+    return fastest_lap
+fastest_lap = get_fast_lap(session=session, driverNumber='1')
+print(fastest_lap)
+
+
 
