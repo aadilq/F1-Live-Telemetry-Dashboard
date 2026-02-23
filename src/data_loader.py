ad6dac021af55b3d1f8f838485ad62833ff7f911
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2026-02-23 17:08:40 -0500

    define function to retrieve telemetry data

--- src/data_loader.py
+++ src/data_loader.py
@@ -13,11 +13,20 @@ def get_session(year, event, session_type):
 # print(session)
 
 def get_fast_lap(session, driverNumber):
-    laps = session.laps.pick_driver(driverNumber)
+    laps = session.laps.pick_drivers(driverNumber)
     fastest_lap = laps.pick_fastest()
     return fastest_lap
+
+
+def get_lap_telemetry(lap):
+    telemetry = lap.get_telemetry()
+    return telemetry
+
+session = get_session(year=2023, event='Monza', session_type='Q')
 fastest_lap = get_fast_lap(session=session, driverNumber='1')
-print(fastest_lap)
+telemetry = get_lap_telemetry(fastest_lap)
+print(telemetry.head())
+print(telemetry.columns)
 
 
 
